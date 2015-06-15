d47110a5afb765776664bd06ba35ac620cf64452
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-15 15:57:29 +0500

    Refactor, add comments.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/emicklei/go-restful"
-	"github.com/emicklei/go-restful/swagger"
 )
 
 func main() {
@@ -13,24 +12,12 @@ func main() {
 	//restful.TraceLogger(log.New(os.Stdout, "[restful] ", log.LstdFlags|log.Lshortfile))
 
 	defer DbSession.Close()
+
 	wsContainer := restful.NewContainer()
 	wsContainer.Router(restful.CurlyRouter{})
-	u := UserResource{map[string]User{}}
-	u.RegisterUserWS(wsContainer)
+
+	RegisterUserWS(wsContainer)
 	RegisterServices(wsContainer)
-
-	// Optionally, you can install the Swagger Service which provides a nice Web UI on your REST API
-	// You need to download the Swagger HTML5 assets and change the FilePath location in the config below.
-	// Open http://localhost:8080/apidocs and enter http://localhost:8080/apidocs.json in the api input field.
-	config := swagger.Config{
-		WebServices:    wsContainer.RegisteredWebServices(), // you control what services are visible
-		WebServicesUrl: "http://localhost:8080",
-		ApiPath:        "/apidocs.json",
-
-		// Optionally, specifiy where the UI is located
-		SwaggerPath:     "/apidocs/",
-		SwaggerFilePath: "/home/you/Projects/go/src/century-link-test/dist"}
-	swagger.RegisterSwaggerService(config, wsContainer)
 
 	log.Printf("start listening on localhost:8080")
 	server := &http.Server{Addr: ":8080", Handler: wsContainer}
